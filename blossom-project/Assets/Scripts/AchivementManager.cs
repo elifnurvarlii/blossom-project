76cf925b4bcc77bf9b8ee166e4d9cafe082c0340
Elifnur Varlı <juniorbloggers@gmail.com>
2024-09-20 16:20:36 +0300

    biseyler

--- blossom-project/Assets/Scripts/AchivementManager.cs
+++ blossom-project/Assets/Scripts/AchivementManager.cs
@@ -1,28 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AchivementManager : MonoBehaviour
 {
 
     public GameObject achivementPrejab;
 
+    public Sprite[] sprites;
+
     private void Start()
     {
-        CreateAchievement("Flower Category");
+        CreateAchievement("Flower Category", "Test" , 1);
     }
 
-    public void CreateAchievement(string category)
+
+    public void CreateAchievement(string category, string title, int spriteIndex)
     {
+
         GameObject achivement = (GameObject)Instantiate(achivementPrejab);
 
-        SetAchievementInfo(category, achivement);
+        SetAchievementInfo(category, achivement, title, spriteIndex);
     }
-         
-    public void SetAchievementInfo(string category, GameObject achivement)
+
+    
+    public void SetAchievementInfo(string category, GameObject achivement, string title, int spriteIndex)
     {
 
         achivement.transform.SetParent(GameObject.Find(category).transform);
+        achivement.transform.localScale = new Vector3(1, 1, 1);
+        achivement.transform.GetChild(0).GetComponent<Image>().sprite = sprites[spriteIndex];
+        achivement.transform.GetChild(1).GetComponent<Text>().text = title;
 
     }
 
