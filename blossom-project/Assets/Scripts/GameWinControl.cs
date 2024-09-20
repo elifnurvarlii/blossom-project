85433db541d25c13dfb3cbc65f9d14df94994700
Elifnur Varlı <juniorbloggers@gmail.com>
2024-09-20 14:51:26 +0300

    game win control

---
+++ blossom-project/Assets/Scripts/GameWinControl.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWinControl : MonoBehaviour
+{
+    public GameObject im1, im2, im3, im4, im5, im6, im7, im8, im9, ph1, ph2, ph3, ph4, ph5, ph6, ph7, ph8, ph9;
+
+    public static bool youWin;
+
+    [SerializeField]
+    private GameObject flowerImage;
+
+    void Update()
+    {
+        if (im1.transform.position == ph1.transform.position && im2.transform.position == ph2.transform.position && im3.transform.position == ph3.transform.position &&
+            im4.transform.position == ph4.transform.position && im5.transform.position == ph5.transform.position && im6.transform.position == ph6.transform.position &&
+            im7.transform.position == ph7.transform.position &&
+            im8.transform.position == ph8.transform.position &&
+            im9.transform.position == ph9.transform.position)
+        {
+
+            youWin = true;
+            flowerImage.SetActive(true);
+
+        }
+
+
+    }
+}
